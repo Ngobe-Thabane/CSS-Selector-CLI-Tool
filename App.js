8a43291bab31b0b9a5b42074bc45fb84a6e26b48
Thabane-Ngobe <tngobe@dvt.co.za>
2025-01-09 10:26:36 +0200

    Added options for node arguments

--- App.js
+++ App.js
@@ -4,17 +4,21 @@ const cherio = require('cheerio');
 const program = new Command();
 
 program
-  .name('URL and a CSS selector')
-  .description('CLI application that takes a URL and a CSS selector arguments and prints the text content of the element that matches the selector.')
-  .version('0.0.1');
+  .name('CSS Attribute Selector')
+  .description(
+    'CLI application that takes a URL and a CSS selector arguments and prints the text content of the element that matches the selector')
+  .version('0.1.0');
 
-program.argument('<string>')
-.option("-s",'--selector');
-  
+program
+.option('--url <string>', "Website URL eg https://somewebsite.com")
+.option('--selector <string>', "CSS selector that you want to print out the content of eg [attribute='value'] ")
+
+
 program.parse();
+const options = program.opts();
 
-axios.get(program.args[0])
-.then(({data}) =>{
+axios.get(options.url)
+.then(({data})=>{
   const $ = cherio.load(data);
-  console.log($(`${program.args[0]}`).text());
-});
+  console.log($(options.selector).text());
+})
